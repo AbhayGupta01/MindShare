981ab02a607563fe1c30c49f0647874b2c51f7e8
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-11 15:43:31 +0530

    initial

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const httpServer = require("http").createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: ["https://socialmediaappserver-e2ws.onrender.com"],
+    origin: ["https://socialmediaappserver-e2ws.onrender.com", "https://post-it-heroku.herokuapp.com"],
   },
 });
 
