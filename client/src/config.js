1ac651e74e90419432f4ea4aa320505c88975f85
AbhayGupta01 <abhaygupta083@gmail.com>
2023-07-11 15:17:58 +0530

    initial

--- client/src/config.js
+++ client/src/config.js
@@ -2,6 +2,6 @@
 // if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
 //   BASE_URL = "http://localhost:4000/";
 // }
-let BASE_URL = "https://socialmediaappserver-e2ws.onrender.com";
+let BASE_URL = "https://socialmediaappserver-e2ws.onrender.com/";
 
 export { BASE_URL };
