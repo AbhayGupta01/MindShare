41ca80dd29b35aeb069dd84562ebe789032d63ca
AbhayGupta01 <abhaygupta083@gmail.com>
2023-12-18 19:34:06 +0530

    repo link

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
         <Typography variant="subtitle1">
           Don't forget to star the{" "}
           <a
-            href="https://github.com/AbhayGupta01/SocialMediaApp"
+            href="https://github.com/AbhayGupta01/MindShare"
             target="_blank"
           >
             Repo
